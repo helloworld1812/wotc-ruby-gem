34e3a9473bf680f4113a9f6fd0d147214f6ec84e
Ryan Lv <xiaorong.ruby@gmail.com>
2020-05-14 15:34:39 +0800

    add timeout

--- lib/wotc/connection.rb
+++ lib/wotc/connection.rb
@@ -18,6 +18,9 @@ module WOTC
 
       Faraday::Connection.new(options) do |conn|
         conn.authorization :Bearer, access_token
+        # https://github.com/lostisland/faraday/issues/417#issuecomment-223413386
+        conn.options[:timeout] = 10 # TODO: move it to configuration
+        conn.options[:open_timeout] = 2 # TODO: move it to configuration
         conn.request :json
 
         conn.use FaradayMiddleWare::RaiseHttpException
--- lib/wotc/version.rb
+++ lib/wotc/version.rb
@@ -1,3 +1,3 @@
 module WOTC
-  VERSION = "0.1.5"
+  VERSION = "0.1.6"
 end
